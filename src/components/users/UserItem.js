b2aaadcb115ee138021766ee81e68c79038c10ac
galoishelley <gracemiao2017@gmail.com>
2021-03-13 17:44:25 +1100

    First commit

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+
+// class UserItem extends Component {
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+
+  // const{login, avatar_url, html_url} = this.props.user;
+
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+
+      <h3>{login}</h3>
+
+      <div>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>More</Link>
+      </div>
+
+    </div>
+  )
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+}
+
+
+
+export default UserItem;
